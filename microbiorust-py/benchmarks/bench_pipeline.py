59a5efeb505e94f515e930733a4ba1de82ab14f7
LCrossman <lisa.crossman@gmail.com>
2025-11-17 11:25:55 +0000

    working on microbiorust-py benchmarking

---
+++ microbiorust-py/benchmarks/bench_pipeline.py
@@ -0,0 +1,29 @@
+import os
+import microbiorust
+
+class PipelineSuite:
+    """
+    Benchmarks for microbiorust-py
+    """
+    # Setup runs before the timer starts
+    def setup(self):
+        bench_dir = os.path.dirname(__file__)
+        
+        # 2. Join it with your filename
+        self.filepath = os.path.join(bench_dir, "Rhiz3841.gbk.gb")
+
+        # Check if it exists just to be safe (helps debugging)
+        if not os.path.exists(self.filepath):
+            raise FileNotFoundError(f"Could not find benchmark file at: {self.filepath}")
+    # 1. Benchmark TIME
+    def time_process_all(self):
+        # This calls your function that returns Vec<String>
+       result = gbk_to_faa(self.filepath)
+       for r in result:
+         print(r)
+
+    # 2. Benchmark MEMORY (The known spike)
+    def peakmem_process_all(self):
+        result = gbk_to_faa(self.filepath)
+        for r in result:
+          print(r)
