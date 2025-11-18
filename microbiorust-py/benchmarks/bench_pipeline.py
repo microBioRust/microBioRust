bba75df05a1d5d1f6cf7f5c2f2e9b1c1bb6db06b
LCrossman <lisa.crossman@gmail.com>
2025-11-18 14:46:39 +0000

    altering asv benchmarking parameters

--- microbiorust-py/benchmarks/bench_pipeline.py
+++ microbiorust-py/benchmarks/bench_pipeline.py
@@ -6,35 +6,23 @@ class PipelineSuite:
     """
     Benchmarks for microbiorust-py
     """
-    # Setup asv parameters - suggested on ArcticDB website
-    rounds = 1
-    number = 1
-    repeat = 2
-    min_run_count = 1
-    warmup_time = 0
     # Setup runs before the timer starts
     def setup(self):
         bench_dir = os.path.dirname(__file__)
         
-        # 2. Join it with your filename
+        # 2. Join it with the test filename
         self.filepath = os.path.join(bench_dir, "Rhiz3841.gbk.gb")
 
         # Check if it exists just to be safe (helps debugging)
         if not os.path.exists(self.filepath):
             raise FileNotFoundError(f"Could not find benchmark file at: {self.filepath}")
-    # 1. Benchmark TIME
+    # 1. Benchmark TIME of protein count function
     def time_process_all(self):
-        # This calls your function that returns Vec<String>
-       print("---- starting run ----", flush=True)
-       t0 = time.time()
-       result = microbiorust.gbk_to_faa(self.filepath)
-       t1 = time.time()
-       print(f"finished parse in {t1-t0:.4f}s", flush=True)
-       #for _ in result:
-       #  pass
-
-    # 2. Benchmark MEMORY (The known spike)
+        # This calls the microBioRust function that returns a count of protein fasta per file
+     
+        _ = microbiorust.gbk_to_faa_count(self.filepath)
+    # 2. Benchmark MEMORY of protein count function
     def peakmem_process_all(self):
-        result = microbiorust.gbk_to_faa(self.filepath)
-        for _ in result:
-          pass
+        
+        _ = microbiorust.gbk_to_faa_count(self.filepath)
+
