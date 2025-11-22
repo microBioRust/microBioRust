36048fc3dc872849064c85b54fe3ca1cda31da97
kh3rld <kherld.hussein@gmail.com>
2025-11-22 17:01:16 -0500

    refactor(lib): reorder module declarations for consistency

--- microBioRust/src/lib.rs
+++ microBioRust/src/lib.rs
@@ -9,6 +9,6 @@
 //!  Additionally, you can create new features and records and save them either in genbank or gff3 format
 //!
 #![allow(non_snake_case)]
-pub mod record;
 pub mod embl;
 pub mod gbk;
+pub mod record;
